5b9aef0988443b53cd8ae90270cc0273d6ca0e3c
JunJie Zhao <790028837@qq.com>
2016-12-11 17:37:42 +0800

    Delete robertsOperator.m

--- Matlab_expe3/robertsOperator.m
+++
@@ -1,7 +0,0 @@
-function outputImg = robertsOperator(inputImg)
-%将RGB图像转为灰度图像
-if(size(inputImg,3) ~= 1)
-    inputImg = rgb2gray(inputImg);
-end
-%Roberts算子分割
-outputImg = im2uint8(edge(inputImg,'roberts'));
