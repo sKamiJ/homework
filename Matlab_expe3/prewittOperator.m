ed055a7c9a1d390f903cb7434766f7cd810df2ef
JunJie Zhao <790028837@qq.com>
2016-12-11 17:37:32 +0800

    Delete prewittOperator.m

--- Matlab_expe3/prewittOperator.m
+++
@@ -1,7 +0,0 @@
-function outputImg = prewittOperator(inputImg)
-%将RGB图像转为灰度图像
-if(size(inputImg,3) ~= 1)
-    inputImg = rgb2gray(inputImg);
-end
-%Prewitt算子分割
-outputImg = im2uint8(edge(inputImg,'prewitt'));
