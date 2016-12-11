9218338b0e9086ad4796426d3189e7601c8ebe41
JunJie Zhao <790028837@qq.com>
2016-12-11 17:36:58 +0800

    Delete cannyOperator.m

--- Matlab_expe3/cannyOperator.m
+++
@@ -1,7 +0,0 @@
-function outputImg = cannyOperator(inputImg)
-%将RGB图像转为灰度图像
-if(size(inputImg,3) ~= 1)
-    inputImg = rgb2gray(inputImg);
-end
-%Canny算子分割
-outputImg = im2uint8(edge(inputImg,'canny'));
